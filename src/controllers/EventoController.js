af9be17f46a4763c7173d6bc365853c8921d3005
Kawan Wagnner <kawanwagnner7@gmail.com>
2024-09-19 16:34:10 -0300

    feat: finish controller

--- src/controllers/EventoController.js
+++ src/controllers/EventoController.js
@@ -42,11 +42,11 @@ const EventController = {
 
   getAll: async (req, res) => {
     try {
-      const clientes = await Evento.findAll();
+      const eventos = await Evento.findAll();
 
       return res.status(200).json({
         msg: "Eventos encontrados!",
-        clientes,
+        eventos,
       });
     } catch (error) {
       console.error(error);
@@ -57,5 +57,44 @@ const EventController = {
     }
   },
 
-  getOne: async (req, res) => {},
+  getOne: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      const eventoUnico = await Evento.findByPk(id);
+
+      if (!eventoUnico) {
+        return res.status(404).json({
+          msg: "Usuário não encontrado!!",
+        });
+      }
+
+      return res.status(200).json({
+        msg: "Usuário encontrado com sucesso!!",
+        evento: eventoUnico,
+      });
+    } catch (error) {
+      console.error(error);
+
+      return res.status(500).json({
+        msg: "Acione o suporte imediamente!!",
+      });
+    }
+  },
+
+  delete: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      const evento = Evento.destroy(id);
+    } catch (error) {
+      console.error(error);
+
+      return res.status(500).json({
+        msg: "Acione o suporte imediatamente!!",
+      });
+    }
+  },
 };
+
+module.exports = EventController;
